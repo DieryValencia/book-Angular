d02481e7de8b172c0052d5debc8a9632f80c4153
DieryValencia <diery.valencia@campusucc.edu.co>
2026-09-23 16:44:51 -0500

    main-list

---
+++ src/app/main-list/main-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-list',
+  imports: [],
+  templateUrl: './main-list.html',
+  styleUrl: './main-list.scss',
+})
+export class MainList {}
